b83e2fe94efd5372c394c42bdfda35dce837376f
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-02-20 16:42:36 +0000

    converting to git auto updating

--- global_vars.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// enter your ZEUS Site API key.
-$config['api_key']				= '1372';
--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-## ZEUS Controller - Update Script
+## ZEUS Controller - Update Script (git)
 
-cd /zeus/controller/
-rm -rf run.sh
-wget -q http://zeus.deltacolo.com/controller/run.sh
-sh run.sh
+cd /zeus
+rm -rf controller/
+git clone ssh://git@github.com/whittinghamj/deltacolo_zeus_controller.git
+mv deltacolo_zeus_controller controller
+cp global_vars.php controller/
+cd controller/
+crontab crontab.txt
+ 
