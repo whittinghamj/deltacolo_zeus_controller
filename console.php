d534a3b8b8fc66aac1969224a08fb5c68ea6c2df
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-02-23 23:10:18 +0000

    dev upgrade to track down Accepted vs Rejected dupe bug

--- console.php
+++ console.php
@@ -227,6 +227,8 @@ if($task == 'update_miner_stats')
 			// get the MAC address
 			// $miner['mac_address'] = exec("nmap -sP ".$miner['ip_address']." | grep MAC");
 
+			echo print_r($miner);
+			
 			$data_string = json_encode($miner);
 
 			$ch = curl_init("http://zeus.deltacolo.com/api/?key=".$config['api_key']."&c=miner_update");                                                                      
