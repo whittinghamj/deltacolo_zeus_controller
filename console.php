ac3dd3829234f7d11c9c58cf2b17cefed828e145
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-02 10:16:50 +0000

    updated ebit for dynamic username and password

--- console.php
+++ console.php
@@ -42,8 +42,8 @@ if($task == 'update_miner_stats')
 			if(ping($miner['ip_address']) == 'alive'){
 				if($miner['hardware'] == 'ebite9' || $miner['hardware'] == 'ebite9plus' || $miner['hardware'] == 'ebite10')
 				{
-					$username 	= 'admin';
-					$password 	= 'admin';
+					$username 	= miner['username'];
+					$password 	= miner['password'];
 					$loginUrl 	= 'http://'.$miner['ip_address'].'/user/login/';
 
 					$ch = curl_init();
