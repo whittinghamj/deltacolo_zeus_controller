27f6fd0d63f74f76334f133c89c736c90f82972b
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-02 15:02:48 +0000

    no message

--- console.php
+++ console.php
@@ -493,10 +493,11 @@ if($task == "site_jobs")
 					$config_file = json_decode($config_file, true);
 
 					$miner_raw = file_get_contents("http://zeus.deltacolo.com/api/?key=".$config['api_key']."&c=site_miner&miner_id=".$site_job['miner']['id']);
-					$miner = json_decode($miner_raw);
+					$miner_bits = json_decode($miner_raw);
 
-					$miner = $miner['miner'];
+					$miner = $miner_bits['miner'];
 
+					echo print_r($miner_bits);
 					echo print_r($miner);
 
 					killlock();
