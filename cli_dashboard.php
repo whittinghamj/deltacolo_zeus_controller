6f5e9cdebb7e0d2a7fd6928713d05e463893738a
Jamie Whittingham (MBA) <jamie.whittingham@gmail.com>
2018-03-04 02:19:12 +0000

    no message

--- cli_dashboard.php
+++ cli_dashboard.php
@@ -9,7 +9,6 @@ $colors = new Colors();
 $data['site']									= file_get_contents('http://zeus.deltacolo.com/api/?key='.$config['api_key'].'&c=site_info');
 $data['site']									= json_decode($data['site'], true);
 
-echo ".:[ SITE }:. \n";
 echo "NAME ......................... " . $data['site']['name' ]. " \n";
 echo "REVENUE / PROFIT ............. " . "$" . $data['site']['monthly_revenue'] . " / " . "$" . $data['site']['monthly_profit'] . " \n";
 echo "AVERAGE TEMP ................. " . $data['site']['average_temps']['average_pcb'] . "°C / " . c_to_f($data['site']['average_temps']['average_pcb']) . "°F \n";
