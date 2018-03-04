7c3060f65ecdd1899e135a75dd9ef71d55e53fee
Jamie Whittingham (MBA) <jamie.whittingham@gmail.com>
2018-03-04 02:12:09 +0000

    no message

--- cli_dashboard.php
+++ cli_dashboard.php
@@ -15,4 +15,16 @@ echo "REVENUE / PROFIT ............. " . "$" . $data['site']['monthly_revenue'] 
 echo "AVERAGE TEMP ................. " . $data['site']['average_temps']['average_pcb'] . "°C / " . c_to_f($data['site']['average_temps']['average_pcb']) . "°F \n";
 echo "POWER: ....................... " . number_format($data['site']['power']['kilowatts'], 2) . " kW / " . number_format($data['site']['power']['amps'], 2) . " AMPs \n";
 
+require_once 'Console/Table.php';
+
+$tbl = new Console_Table();
+
+$tbl->setHeaders(array('ID', 'Name', 'IP', 'Hashrate'));
+
+foreach($data['site']['miners'] as $miner)
+{
+	$tbl->addRow(array($miner['id'], $miner['name'], $miner['ip_address'], $miner['hashrate']));
+}
+
+echo $tbl->getTable();
 ?>
