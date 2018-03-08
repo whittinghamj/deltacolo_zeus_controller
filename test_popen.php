5f465b95c700992255ca67f64a427462cc177804
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-08 22:43:45 +0000

    no message

--- test_popen.php
+++ test_popen.php
@@ -4,7 +4,7 @@
 for ($i=0; $i<10; $i++) {
     // open ten processes
     for ($j=0; $j<10; $j++) {
-        $pipe[$j] = popen('php -q show_time.php', 'w');
+        $pipe[$j] = popen('php -q show_time.php', 'r');
     }
 
     // wait for them to finish
