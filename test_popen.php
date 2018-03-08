3a5cec3f61275c6dbc0f8035260c69c7c6ba45ba
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-08 23:49:52 +0000

    no message

--- test_popen.php
+++ test_popen.php
@@ -3,16 +3,18 @@
 
 $miners = array('28283', '28284', '28285', '28286', '28287', '28288', '28328');
 
-for ($i=0; $i<count(miners); $i++) {
+$count = count($miners);
+
+for ($i=0; $i<$count; $i++) {
     // open ten processes
-    for ($j=0; $j<count(miners); $j++) {
+    for ($j=0; $j<$count; $j++) {
     	echo "Checking Miner: ".$miners[$j]."\n";
 
         $pipe[$j] = popen("php -q get_miner_stats.php -p='".$miners[$j]."'", 'w');
     }
 
     // wait for them to finish
-    for ($j=0; $j<count(miners); ++$j) {
+    for ($j=0; $j<$count; ++$j) {
         pclose($pipe[$j]);
     }
 }
