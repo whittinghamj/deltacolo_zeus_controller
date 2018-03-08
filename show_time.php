9fe2d2239ee64eae9b132c661762fc7aa9fd7f70
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-08 23:08:16 +0000

    no message

--- show_time.php
+++ show_time.php
@@ -2,9 +2,9 @@
 
 echo print_r($argv);
 
-$options = getopt("p:");
-$part = $options["p"];
+$options = getopt("miner_id:");
+$miner_id = $options["miner_id"];
 
 echo print_r($options);
 
-exec("touch ".time()."-".$part.".txt");
+exec("touch ".time()."-".$miner_id.".txt");
