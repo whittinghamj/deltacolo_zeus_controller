654e894ad6c991187dfa945f014395b86e86f856
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-09 00:11:41 +0000

    no message

--- test_popen.php
+++ test_popen.php
@@ -1,10 +1,18 @@
 <?php
 
+// version 0.1.2
+
+include('global_vars.php');
+include('functions.php');
+
 $runs = $argv[1];
 
-$miners = array('28283', '28284', '28285', '28286', '28287', '28288', '28328');
+$miners_raw 	= file_get_contents("http://zeus.deltacolo.com/api/?key=".$config['api_key']."&c=site_miners");
+$miners 		= json_decode($miners_raw, true);
+$miners 		= $miners['miners'];
+$count 			= count($miners);
 
-$count = count($miners);
+console_output("Polling " . $count . " miners.");
 
 for ($i=0; $i<$runs; $i++) {
     // open ten processes
