3806361d6dabfd7209f7c3a1cff9a9a987f26952
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-09 00:49:19 +0000

    no message

--- update.sh
+++ update.sh
@@ -3,7 +3,8 @@
 ## ZEUS Controller - Update Script (git pull)
 
 git --git-dir=/zeus/controller/.git pull origin master
-crontab /zeus/controller/crontab.txt
 
 rm -rf /etc/rc.local
 mv /zeus/controller/rc.local /etc
+
+crontab /zeus/controller/crontab.txt
