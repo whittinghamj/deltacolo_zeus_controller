67cca85158c14cfaaad12127ef691a1e8834e4da
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-09 00:18:26 +0000

    no message

--- test_popen.php
+++ test_popen.php
@@ -7,19 +7,28 @@ include('functions.php');
 
 $runs = $argv[1];
 
-$miners_raw 	= file_get_contents("http://zeus.deltacolo.com/api/?key=".$config['api_key']."&c=site_miners");
-$miners 		= json_decode($miners_raw, true);
-$miners 		= $miners['miners'];
-$count 			= count($miners);
+$miners_raw 		= file_get_contents("http://zeus.deltacolo.com/api/?key=".$config['api_key']."&c=site_miners");
+$miners 			= json_decode($miners_raw, true);
+
+foreach($miners['miners'] as $miner)
+{
+	$miner_ids[] = $miner['id'];
+}
+
+$count 				= count($miner_ids);
+
+echo print_r($miner_ids);
 
 console_output("Polling " . $count . " miners.");
+
+die();
 
 for ($i=0; $i<$runs; $i++) {
     // open ten processes
     for ($j=0; $j<$count; $j++) {
-    	echo "Checking Miner: ".$miners[$j]."\n";
+    	echo "Checking Miner: ".$miners['miners'][$j]."\n";
 
-        $pipe[$j] = popen("php -q get_miner_stats.php -p='".$miners[$j]."'", 'w');
+        $pipe[$j] = popen("php -q get_miner_stats.php -p='".$miners['miners'][$j]."'", 'w');
     }
 
     // wait for them to finish
