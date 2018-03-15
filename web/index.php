d10fbc7fa67c356c4920900f33138253b6b13502
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-15 16:41:51 +0000

    no message

--- web/index.php
+++ web/index.php
@@ -209,71 +209,71 @@ desired effect
 						<div id="chart3_div" class="chart half"></div>
                    	
                    		<script>
-							      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+							  google.charts.load('current', {'packages':['corechart']});
+							  google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
-        var netdata = 'http://192.168.0.46:19999/api/v1/data?after=-60&format=datasource&options=nonzero&chart=';
-        
-        // define all the charts you need
-      	var mycharts = [
-        	{
-          	div: 'chart1_div',
-            chart: 'system.cpu',
-            type: 'stacked',
-            options: { // these are google chart options
-              title: 'System CPU',
-              vAxis: {minValue: 100}
-            }
-          },
-        	{
-          	div: 'chart2_div',
-            chart: 'system.io',
-            type: 'area',
-            options: { // these are google chart options
-              title: 'System I/O',
-            }
-          },
-        	{
-          	div: 'chart3_div',
-            chart: 'ipv4.tcpsock',
-            type: 'line',
-            options: { // these are google chart options
-              title: 'TCP sockets',
-            }
-          }
-        ];
+							  function drawChart() {
+								var netdata = 'http://192.168.0.46:19999/api/v1/data?after=-60&format=datasource&options=nonzero&chart=';
 
-				// initialize the google charts
-        var len = mycharts.length;
-        while(len--) {
-        	mycharts[len].query = new google.visualization.Query(netdata + mycharts[len].chart, {sendMethod: 'auto'});
-          
-          switch(mycharts[len].type) {
-          	case 'stacked':
-              mycharts[len].options.isStacked = 'absolute';
-              // no break here - render it as area chart
-          	case 'area':
-		          mycharts[len].gchart = new google.visualization.AreaChart(document.getElementById(mycharts[len].div));
-              break;
-              
-            default:
-		          mycharts[len].gchart = new google.visualization.LineChart(document.getElementById(mycharts[len].div));
-            	break;
-          }
-        }
-        
-        function refreshChart(c) {
-        	c.query.send(function(data) {
-            c.gchart.draw(data.getDataTable(), c.options);
-          });
-        }
-        
-				setInterval(function() {
-          var len = mycharts.length;
-          while(len--) refreshChart(mycharts[len]);
-        }, 1000);
-      }
+								// define all the charts you need
+								var mycharts = [
+									{
+									div: 'chart1_div',
+									chart: 'system.cpu',
+									type: 'stacked',
+									options: { // these are google chart options
+									  title: 'System CPU',
+									  vAxis: {minValue: 100}
+									}
+								  },
+									{
+									div: 'chart2_div',
+									chart: 'system.ram',
+									type: 'area',
+									options: { // these are google chart options
+									  title: 'System RAM',
+									}
+								  },
+									{
+									div: 'chart3_div',
+									chart: 'ipv4.tcpsock',
+									type: 'line',
+									options: { // these are google chart options
+									  title: 'TCP sockets',
+									}
+								  }
+								];
+
+										// initialize the google charts
+								var len = mycharts.length;
+								while(len--) {
+									mycharts[len].query = new google.visualization.Query(netdata + mycharts[len].chart, {sendMethod: 'auto'});
+
+								  switch(mycharts[len].type) {
+									case 'stacked':
+									  mycharts[len].options.isStacked = 'absolute';
+									  // no break here - render it as area chart
+									case 'area':
+										  mycharts[len].gchart = new google.visualization.AreaChart(document.getElementById(mycharts[len].div));
+									  break;
+
+									default:
+										  mycharts[len].gchart = new google.visualization.LineChart(document.getElementById(mycharts[len].div));
+										break;
+								  }
+								}
+
+								function refreshChart(c) {
+									c.query.send(function(data) {
+									c.gchart.draw(data.getDataTable(), c.options);
+								  });
+								}
+
+										setInterval(function() {
+								  var len = mycharts.length;
+								  while(len--) refreshChart(mycharts[len]);
+								}, 1000);
+							  }
       
 						</script>
                     	
