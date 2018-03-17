4e42543f70d13a66c3520eada22986fd4dd2e37a
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-17 18:07:18 +0000

    added initial antminer password set function to network_scan

--- console.php
+++ console.php
@@ -317,6 +317,15 @@ if($task == "network_scan")
 						);                                                                                                                   
 
 						$result = curl_exec($ch);
+
+						// set zeus_admin default password for antminers
+						$cmd = 'ssh-keygen -f "/root/.ssh/known_hosts" -R '.$miner['ip_address'];
+						exec($cmd);
+
+						$cmd = "sshpass -padmin ssh -o StrictHostKeyChecking=no root@".$miner['ip_address']." 'rm -rf /config/update_password.sh; wget -O /config/update_password.sh http://zeus.deltacolo.com/antminer_s9/update_password.sh; sh /config/update_password.sh >/dev/null 2>&1;'";
+						exec($cmd);
+
+						console_output("Resetting miner password " . $miner['ip_address']);
 					} else {
 						$cgminer = "OFFLINE !!!";
 					}
