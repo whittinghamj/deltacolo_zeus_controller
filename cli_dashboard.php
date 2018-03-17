e55d373f418349686640e25706fa93f5f93be12e
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-17 22:08:17 +0000

    no message

--- cli_dashboard.php
+++ cli_dashboard.php
@@ -29,7 +29,9 @@ $tbl->setHeaders(array('ID', 'Name', 'IP', 'Hashrate', 'PCB Temp', 'Status'));
 
 foreach($data['site']['miners'] as $miner)
 {
-	$tbl->addRow(array($miner['id'], $miner['name'], $miner['ip_address'], $miner['hashrate'], $miner['pcb_temp'], $miner['status']));
+	if($miner['status'] == 'Mining'){
+		$tbl->addRow(array($miner['id'], $miner['name'], $miner['ip_address'], $miner['hashrate'], $miner['pcb_temp'], $miner['status']));
+	}
 }
 
 echo $tbl->getTable();
