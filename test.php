f783c4a1e9cf5087bcdaf8a48e07a9cfb2108951
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-26 12:19:42 +0100

    no message

--- test.php
+++ test.php
@@ -13,7 +13,7 @@ echo "Miner IP: ".$ip_address." \n";
 echo "New Password: ".$new_password." \n";
 echo "Password Hash: ".$password_hash." \n";
 
-$cmd = "sshpass -padmin ssh -o StrictHostKeyChecking=no root@".$ip_address." 'echo -e \"admin1372\nadmin1372\" | passwd root > /dev/nul; rm -f /config/shadow; mv /etc/shadow /config/shadow; ln -s /config/shadow /etc/shadow'";
+$cmd = "sshpass -padmin ssh -o StrictHostKeyChecking=no root@".$ip_address." 'echo -e \"".$new_password."\n".$new_password."\" | passwd root > /dev/nul; rm -f /config/shadow; mv /etc/shadow /config/shadow; ln -s /config/shadow /etc/shadow'";
 exec($cmd);
 
 echo "Setting password for root@" . $ip_address . " to " . $new_password. " \n";
