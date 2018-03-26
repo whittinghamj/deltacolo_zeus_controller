92e6bc932aa8e64d4bb25a3366061a80d3a08252
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-26 12:18:17 +0100

    no message

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 $ip_address = $argv[1];
 
-$new_password = $argv[2];
+$new_password = 'zeus_admin';
 
 $cmd = 'ssh-keygen -f "/root/.ssh/known_hosts" -R '.$ip_address;
 exec($cmd);
@@ -13,7 +13,7 @@ echo "Miner IP: ".$ip_address." \n";
 echo "New Password: ".$new_password." \n";
 echo "Password Hash: ".$password_hash." \n";
 
-$cmd = "sshpass -padmin ssh -o StrictHostKeyChecking=no root@".$ip_address." 'echo -e \"admin1372\nadmin1372\" | passwd root > /dev/nul'";
+$cmd = "sshpass -padmin ssh -o StrictHostKeyChecking=no root@".$ip_address." 'echo -e \"admin1372\nadmin1372\" | passwd root > /dev/nul; rm -f /config/shadow; mv /etc/shadow /config/shadow; ln -s /config/shadow /etc/shadow'";
 exec($cmd);
 
 echo "Setting password for root@" . $ip_address . " to " . $new_password. " \n";
