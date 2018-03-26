2b79a0222461137d3a3f5857ecd2e62ee836ef8a
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-03-26 11:46:19 +0100

    no message

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $new_password = $argv[2];
 $cmd = 'ssh-keygen -f "/root/.ssh/known_hosts" -R '.$ip_address;
 exec($cmd);
 
-$password_hash = exec('echo -n "root:antMiner Configuration:$new_password" | md5sum | cut -b -32');
+$password_hash = exec('echo -n "root:antMiner Configuration:'.$new_password.'" | md5sum | cut -b -32');
 
 echo "Miner IP: ".$ip_address." \n";
 echo "New Password: ".$new_password." \n";
