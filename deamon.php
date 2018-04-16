08ecf4035a4fedd51d9dde8d0684e9bbef99835a
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-04-16 16:24:05 +0100

    no message

--- deamon.php
+++ deamon.php
@@ -5,9 +5,9 @@
 include('/zeus/controller/global_vars.php');
 include('/zeus/controller/functions.php');
 
-console_output("Building deamon. May take up to 60 seconds.");
+console_output("Building deamon. May take up to 30 seconds.");
 
-sleep(60);
+sleep(30);
 
 $runs = $argv[1];
 
