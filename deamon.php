1eaf205b35c7f612e3fed67994be4b99ad10f2cf
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-29 22:06:57 +0100

    no message

--- deamon.php
+++ deamon.php
@@ -18,31 +18,36 @@ $runs = $argv[1];
 $miners_raw 		= file_get_contents($api_url."/api/?key=".$config['api_key']."&c=site_miners");
 $miners 			= json_decode($miners_raw, true);
 
-foreach($miners['miners'] as $miner)
+if(is_array($miners))
 {
-	$miner_ids[] = $miner['id'];
-}
-
-$count 				= count($miner_ids);
-
-console_output("Polling " . $count . " miners.");
-
-for ($i=0; $i<$runs; $i++) {
-    console_output("Spawning children.");
-    for ($j=0; $j<$count; $j++) {
-    	echo "Checking Miner: ".$miner_ids[$j]."\n";
-
-        $pipe[$j] = popen("php -q /mcp/deamon_update_miner_stats.php -p='".$miner_ids[$j]."'", 'w');
+    foreach($miners['miners'] as $miner)
+    {
+    	$miner_ids[] = $miner['id'];
     }
 
-    console_output("Killing children.");
-    // wait for them to finish
-    for ($j=0; $j<$count; ++$j) {
-        pclose($pipe[$j]);
-    }
+    $count 				= count($miner_ids);
 
-    // console_output("Sleeping.");
-    // sleep(1);
+    console_output("Polling " . $count . " miners.");
+
+    for ($i=0; $i<$runs; $i++) {
+        console_output("Spawning children.");
+        for ($j=0; $j<$count; $j++) {
+        	echo "Checking Miner: ".$miner_ids[$j]."\n";
+
+            $pipe[$j] = popen("php -q /mcp/deamon_update_miner_stats.php -p='".$miner_ids[$j]."'", 'w');
+        }
+
+        console_output("Killing children.");
+        // wait for them to finish
+        for ($j=0; $j<$count; ++$j) {
+            pclose($pipe[$j]);
+        }
+
+        // console_output("Sleeping.");
+        // sleep(1);
+    }
+}else{
+    console_output("No ASIC miners.");
 }
 
 exit();
