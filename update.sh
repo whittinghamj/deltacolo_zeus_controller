a5ee3efe0bff360c7763a803967a54244ff40ab2
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-09-29 21:13:17 +0100

    no message

--- update.sh
+++ update.sh
@@ -2,8 +2,12 @@
 
 ## MCP Controller - Update Script (git pull)
 
+mv /mcp/global_vars.php /mcp/global_vars.tmp
+
 cd /mcp && git --git-dir=/mcp/.git pull origin master
 
 crontab /mcp/crontab.txt
 
-chmod 777 global_vars.php
+mv /mcp/global_vars.tmp /mcp/global_vars.php
+
+chmod 777 /mcp/global_vars.php
