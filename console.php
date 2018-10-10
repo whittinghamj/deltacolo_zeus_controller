41c5be3ac035e1414647a0e7730d117dbfcd3c27
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-10 21:53:55 +0100

    no message

--- console.php
+++ console.php
@@ -555,8 +555,7 @@ if($task == "site_jobs")
 					$stats = json_decode($content, TRUE);
 
 				}
-				elseif(
-					if (strpos($site_job['miner']['hardware'], 'antminer-s9') !== false) {
+				elseif(strpos($site_job['miner']['hardware'], 'antminer-s9') !== false) {
 					echo "Hardware: Bitmain Antminer S9 \n";
 					echo "Downloading: ".$api_url."/miner_config_files/".$site_job['miner']['id'].".txt \n";
 					shell_exec("sshpass -p".$site_job['miner']['password']." ssh -o StrictHostKeyChecking=no ".$site_job['miner']['username']."@".$site_job['miner']['ip_address']." 'rm -rf /config/bmminer.conf; wget -O /config/bmminer.conf ".$api_url."/miner_config_files/".$site_job['miner']['id'].".txt; /etc/init.d/bmminer.sh restart >/dev/null 2>&1;'");
