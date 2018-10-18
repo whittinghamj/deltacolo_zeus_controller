e29f70431ca0da06b50d0b12379d4db970da7532
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-18 10:20:23 +0100

    no message

--- test.sh
+++ test.sh
@@ -2,5 +2,9 @@
 
 SITE_API_KEY="$1"
 
-content=$(curl -sS "http://dashboard.miningcontrolpanel.com/api/?key=$SITE_API_KEY&c=site_ip_ranges")
-echo $content
+JSON_DATA=$(curl -sS "http://dashboard.miningcontrolpanel.com/api/?key=$SITE_API_KEY&c=site_ip_ranges")
+echo $JSON_DATA
+
+SITE_NAME=`echo "$JSON_DATA" | jq -r .site.name`
+
+echo "Site Name: " $SITE_NAME
