6d88ef03d14eb76c4f19b1fd760186268a9e042a
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-18 10:45:23 +0100

    no message

--- ip_scanner.sh
+++ ip_scanner.sh
@@ -4,18 +4,18 @@
 ip_range=$1'0/24'
 
 ## remove files from last run
-rm -rf /mcp/online_ip_addresses.txt
+#rm -rf /mcp/online_ip_addresses.txt
 
 ## create new files for this run
-touch /mcp/online_ip_addresses.txt
+#touch /mcp/online_ip_addresses.txt
 
 ## get all ip addresses for this subnet running cgminer / bmminer api
-#echo "Scanning $ip_range"
+echo "Scanning $ip_range"
 	nmap -p4028 $ip_range -oG - | grep 4028/open | awk '{ print $2 }' >> /mcp/online_ip_addresses.txt
-#echo "Done."
+echo "Done."
 
-#echo ""
+echo ""
 
 active_miners=`cat /mcp/online_ip_addresses.txt | wc -l`
 
-#echo "Found $active_miners miners on subnet $ip_range"
+echo "Found $active_miners miners on subnet $ip_range"
