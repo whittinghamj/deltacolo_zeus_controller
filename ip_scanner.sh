5e707af1bcc677c3624f9d9dd17fc8d1e991d1a1
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-18 06:59:26 +0100

    no message

--- ip_scanner.sh
+++ ip_scanner.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+## remove files from last run
+rm -rf /mcp/all_ip_addresses.txt
+rm -rf /mcp/online_ip_addresses.txt
+
+## create new files for this run
+touch /mcp/all_ip_addresses.txt
+touch /mcp/online_ip_addresses.txt
+
 ## get all ip addresses for this subnet
 echo "Building IP address range."
-	nmap -n -sL 192.168.7.0/24 | grep "Nmap scan report" | awk '{print $NF}' > dev_ips.txt
+	nmap -n -sL 192.168.7.0/24 | grep "Nmap scan report" | awk '{print $NF}' > all_ip_addresses.txt
 echo "Done."
 
 echo ""
 
 ## check each ip address to see if its online
 echo "Scanning IP addresses for active hosts"
-	parallel -j 128 -a dev_ips.txt 'ping -c1 {} > /dev/null 2>&1 && echo {} is available || echo {} is down'
+	parallel -j 128 -a dev_ips.txt 'ping -c1 {} > /dev/null 2>&1 && echo {} >> online_ip_addresses.txt || '
 echo "Done."
